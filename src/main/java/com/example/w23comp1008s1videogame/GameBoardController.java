58a734714366047f18cdbc605623c31ef58b8c09
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-03-23 17:51:49 -0400

    Week 10 - Added the sprite and controller class

--- src/main/java/com/example/w23comp1008s1videogame/GameBoardController.java
+++ src/main/java/com/example/w23comp1008s1videogame/GameBoardController.java
@@ -1,4 +1,47 @@
 package com.example.w23comp1008s1videogame;
 
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+
 public class GameBoardController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void startGame(ActionEvent event) {
+        startButton.setVisible(false);
+
+        Canvas canvas = new Canvas(1000,800);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //get Image objects
+        Image shipImage = new Image(getClass().getResourceAsStream("images/ship.png"));
+        Image background = new Image(getClass().getResourceAsStream("images/space.png"));
+
+        //Create a Sprite that we can draw on our canvas
+        Sprite ship = new Sprite (0,500,100,70,10,shipImage);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                gc.drawImage(background,0,0,1000,800);
+                ship.draw(gc);
+                ship.moveRight();
+            }
+        };
+        timer.start();
+        anchorPane.getChildren().add(canvas);
+
+    }
+
 }
