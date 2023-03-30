1c68d3a38491725b3be2d9af43efa6313fff13b1
JaretWright <jaret.wright@georgiancollege.ca>
2023-03-30 16:14:07 -0400

    Week 11 - Created the Missile.java class

---
+++ src/main/java/com/example/w23comp1008s1videogame/Missile.java
@@ -0,0 +1,28 @@
+package com.example.w23comp1008s1videogame;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Missile extends Sprite{
+
+    /**
+     * We know the missile image, width, height and speed
+     * @param posX
+     * @param posY
+     */
+    public Missile(int posX, int posY) {
+        super(posX, posY, 40, 20, 7,
+                new Image(Missile.class.getResourceAsStream("images/missile.png")));
+    }
+
+    private void moveRight()
+    {
+        posX += speed;
+    }
+
+    public void draw(GraphicsContext gc)
+    {
+        super.draw(gc);
+        moveRight();
+    }
+}
